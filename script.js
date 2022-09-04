2fc50f175ddcab3fb521b8614b588f5f104141b0
Bhuvana BK <73237716+bhuvanabk123@users.noreply.github.com>
2022-09-04 18:55:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+function displayValue(e) {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
+
+
+        if (action === 'add' || action === 'subtract' ||
+            action === 'multiply' || action === 'divide') {
+
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType != 'calculate') {
+                const calcValue = calculate(firstValue, operator, secondValue);
+                display.textContent = calcValue;
+                calculator.dataset.firstValue = calcValue;
+            } else {
+                calculator.dataset.firstValue = displayedNum;
+            }
+
+
+            key.classList.add('is-depressed');
+            calculator.dataset.operator = action;
+            calculator.dataset.previousKeyType = 'operator';
+        }
+        if (action === 'decimal') {
+
+            console.log('decimal key!');
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.';
+            } else if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = '0.';
+            }
+            calculator.dataset.previousKeyType = 'decimal';
+        }
+
+
+        if (action !== 'clear') {
+            const clearButton = calculator.querySelector('[data-action=clear]');
+            clearButton.textContent = 'CE';
+        }
+
+
+        if (action === 'clear') {
+
+
+            if (key.textContent === 'AC') {
+                calculator.dataset.firstValue = '';
+                calculator.dataset.modValue = '';
+                calculator.dataset.operator = '';
+                calculator.dataset.previousKeyType = '';
+            } else {
+                key.textContent = 'AC';
+            }
+            display.textContent = 0;
+            calculator.dataset.previousKeyType = 'clear';
+        }
+
+
+
+
+        if (action === 'calculate') {
+            let firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            let secondValue = displayedNum;
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayedNum;
+                    secondValue = calculator.dataset.modValue;
+                }
+                display.textContent = calculate(firstValue, operator, secondValue);
+
+            }
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = 'calculate';
+        }
+
+
+
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+            calculator.dataset.previousKeyType = 'number';
+        }
+    }
+}
+
+
+
+const calculate = (n1, operator, n2) => {
+    let result = '';
+    if (operator === 'add') return  result = parseFloat(n1) + parseFloat(n2); 
+    if (operator === 'subtract') return   result = parseFloat(n1) - parseFloat(n2);
+    if (operator === 'multiply') return   result = parseFloat(n1) * parseFloat(n2);
+    if (operator === 'divide')  return   result = parseFloat(n1) / parseFloat(n2);    
+}
+
+const display = document.querySelector(".display");
+const calculator = document.querySelector(".container");
+const keys = calculator.querySelector(".calculator_keys");
+keys.addEventListener('click', displayValue);
+
+
